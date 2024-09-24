8c53da40f7e57fdbdcea9af97ac261b1a1282c2b
Magicolo <magicololand@gmail.com>
2024-09-24 02:40:37 +0000

    fixed test

--- tests/attribute.rs
+++ tests/attribute.rs
@@ -46,6 +46,7 @@ fn compiles_with_regex_input(value: String) {
 }
 
 #[check(Generate::collect('a'..='z'), Generate::collect('A'..='Z'))]
+#[should_panic]
 fn fails_on_specific_input(left: String, right: String) {
     if left.len() + right.len() > 10 {
         assert_eq!(left.contains('z'), right.contains('Z'));
