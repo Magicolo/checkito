5cfa4068c45220341deae21b3b6cef8ee9f36bf4
Magicolo <magicololand@gmail.com>
2024-10-02 05:01:59 +0000

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -38,15 +38,6 @@ use core::{
 };
 use primitive::Range;
 
-/*
-    TODO: Improve test coverage dramatically.
-    FIXME: #[check] macro produces duplicate compile errors (see 'Excess expression').
-    TODO: Review clamping of `size` in `Size` and `Dampen`.
-        - Should they be allowed to go outside the range?
-        - If `size` is set to a fixed value (ex: #[check(size = 1.0)]), then `Dampen` cannot prevent exponential
-        growth of recursive structures.
-*/
-
 pub fn number<T>() -> impl Generate<Item = T>
 where
     Range<T>: Generate<Item = T>,
