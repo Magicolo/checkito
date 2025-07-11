17e5ed84909bf8a880319f404cebd72730f085e4
Magicolo <magicololand@gmail.com>
2025-07-11 04:14:38 +0000

    refactor: Simplify cardinality methods and update LazyCell and LazyLock implementations

--- src/lazy.rs
+++ src/lazy.rs
@@ -20,44 +20,40 @@ impl<G: Generate, F: Fn() -> G> Generate for Lazy<G, F> {
     }
 
     fn cardinality(&self) -> Option<u128> {
-        self.0.get_or_init(|| self.1()).cardinality()
+        self.0.get().map_or(G::CARDINALITY, G::cardinality)
     }
 }
 
 #[rustversion::since(1.80)]
-use core::cell::LazyCell;
-#[rustversion::since(1.80)]
 #[allow(clippy::incompatible_msrv)]
-impl<G: Generate, F: FnOnce() -> G> Generate for LazyCell<G, F> {
+impl<G: Generate, F: FnOnce() -> G> Generate for core::cell::LazyCell<G, F> {
     type Item = G::Item;
     type Shrink = G::Shrink;
 
     const CARDINALITY: Option<u128> = G::CARDINALITY;
 
     fn generate(&self, state: &mut crate::state::State) -> Self::Shrink {
-        LazyCell::force(self).generate(state)
+        Self::force(self).generate(state)
     }
 
     fn cardinality(&self) -> Option<u128> {
-        LazyCell::force(self).cardinality()
+        Self::force(self).cardinality()
     }
 }
 
 #[rustversion::since(1.80)]
-use std::sync::LazyLock;
-#[rustversion::since(1.80)]
 #[allow(clippy::incompatible_msrv)]
-impl<G: Generate, F: FnOnce() -> G> Generate for LazyLock<G, F> {
+impl<G: Generate, F: FnOnce() -> G> Generate for std::sync::LazyLock<G, F> {
     type Item = G::Item;
     type Shrink = G::Shrink;
 
     const CARDINALITY: Option<u128> = G::CARDINALITY;
 
     fn generate(&self, state: &mut crate::state::State) -> Self::Shrink {
-        LazyLock::force(self).generate(state)
+        Self::force(self).generate(state)
     }
 
     fn cardinality(&self) -> Option<u128> {
-        LazyLock::force(self).cardinality()
+        Self::force(self).cardinality()
     }
 }
